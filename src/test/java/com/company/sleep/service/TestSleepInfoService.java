9ff2eca29689fd6e7442e188a1ebb11421c5eff1
rblcoder <35212670+rblcoder@users.noreply.github.com>
2022-05-20 17:59:09 +0530

    add service tests

--- src/test/java/com/company/sleep/service/TestSleepInfoService.java
+++ src/test/java/com/company/sleep/service/TestSleepInfoService.java
@@ -9,6 +9,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -39,6 +41,21 @@ public class TestSleepInfoService {
     }
 
     @Test
+    void givenId_whenFindById_thenReturnRecordById() {
+        SleepInfo sleepInfo = SleepInfo.builder()
+                .id(1L)
+                .sleepDateTime(Timestamp.valueOf("2022-01-01 21:20:00"))
+                .build();
+
+        when(sleepInfoRepository.findById(1L)).thenReturn(Optional.ofNullable(sleepInfo));
+
+        SleepInfo actualEntry = sleepInfoService.getEntryById(1L);
+
+        assertEquals(sleepInfo, actualEntry);
+
+    }
+
+    @Test
     void givenNewRecord_whenSaveRecord_thenReturnUpdatedRecordWithHoursCalculated() {
         SleepInfo dbsleepInfo = SleepInfo.builder()
                 .id(1L)
@@ -63,6 +80,32 @@ public class TestSleepInfoService {
         SleepInfo actualEntry = sleepInfoService.updateEntry(sleepInfo, 1L);
 
         assertEquals(saveSleepInfo, actualEntry);
+
+    }
+
+    @Test
+    void whenFindAllRecords_thenReturnAllRecords() {
+        SleepInfo dbsleepInfoFirstRecord = SleepInfo.builder()
+                .id(1L)
+                .sleepDateTime(Timestamp.valueOf("2022-01-01 21:20:00"))
+                .getUpDateTime(Timestamp.valueOf("2022-01-02 05:20:00"))
+                .hours(8L)
+                .build();
+
+        SleepInfo dbsleepInfoSecondRecord = SleepInfo.builder()
+                .id(2L)
+                .sleepDateTime(Timestamp.valueOf("2022-01-02 21:20:00"))
+                .build();
+
+        List<SleepInfo> sleepInfoList = new ArrayList<>();
+        sleepInfoList.add(dbsleepInfoFirstRecord);
+        sleepInfoList.add(dbsleepInfoSecondRecord);
+
+        when(sleepInfoRepository.findAll()).thenReturn(sleepInfoList);
+
+        List<SleepInfo> actualSleepInfoList = sleepInfoService.getAllEntries();
+
+        assertEquals(sleepInfoList, actualSleepInfoList);
 
     }
 }
