6cfba974eafcb94c84d67416c8ba673eec977c0e
rblcoder <35212670+rblcoder@users.noreply.github.com>
2022-05-20 17:45:33 +0530

    h2 database for tests

--- src/test/java/com/company/sleep/SleepApplicationTests.java
+++ src/test/java/com/company/sleep/SleepApplicationTests.java
@@ -2,8 +2,10 @@ package com.company.sleep;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.yml")
 class SleepApplicationTests {
 
 	@Test
