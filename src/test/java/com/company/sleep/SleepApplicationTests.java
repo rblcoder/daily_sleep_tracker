76e790357eea856cb5d1f63612c6e8efe30ee9a1
rblcoder <35212670+rblcoder@users.noreply.github.com>
2022-05-20 12:41:36 +0530

    initial commit

---
+++ src/test/java/com/company/sleep/SleepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.sleep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/company/sleep/controller/TestHomeController.java
@@ -0,0 +1,29 @@
+package com.company.sleep.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(controllers = HomeController.class)
+public class TestHomeController {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    void shouldLoadHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Sleep Tracker")));
+    }
+
+}
