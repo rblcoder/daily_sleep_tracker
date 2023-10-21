d310796018b26181f5d73d7a9a5e1ffef2c638e3
Rupa Lahiri <rupacoach@gmail.com>
2023-10-21 18:20:18 +0530

    Update SleepInfoController.java

--- src/main/java/com/company/sleep/controller/SleepInfoController.java
+++ src/main/java/com/company/sleep/controller/SleepInfoController.java
@@ -15,6 +15,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Objects;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class SleepInfoController {
@@ -92,6 +102,16 @@ public class SleepInfoController {
         sleepInfoService.deleteEntryById(Id);
 
         return "redirect:/";
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+      File convertFile = new File("/var/tmp/"+file.getOriginalFilename());
+      convertFile.createNewFile();
+      FileOutputStream fout = new FileOutputStream(convertFile);
+      fout.write(file.getBytes());
+      fout.close();
+      return "File is upload successfully";
     }
 
 }
